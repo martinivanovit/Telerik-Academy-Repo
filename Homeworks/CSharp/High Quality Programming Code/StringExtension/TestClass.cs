11d6ae4657c84e7ecf668bb3776f0ef52aa93ea4
Martin Ivanov <martinivanov.it@gmail.com>
2013-05-13 16:16:49 +0300

    String extension added
    New homework from HQPC course - about documentation and commenting

---
+++ Homeworks/CSharp/High Quality Programming Code/StringExtension/TestClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace Telerik.ILS.Common
+{
+    class TestClass
+    {
+        static void Main(string[] args)
+        {
+            //You can test the extensions here
+        }
+    }
+}
